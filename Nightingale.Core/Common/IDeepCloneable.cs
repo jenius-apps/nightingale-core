1a3c81bbee5fd57dc694eff49b93ca9a968ed10c
Daniel Paulino <d_paulino@outlook.com>
2020-05-03 23:51:49 -0700

    Added common

---
+++ Nightingale.Core/Common/IDeepCloneable.cs
@@ -0,0 +1,16 @@
+﻿namespace Nightingale.Core.Common
+{
+    /// <summary>
+    /// Interface for performing a deep clone
+    /// of an object.
+    /// </summary>
+    /// <typeparam name="T">The type of the object to clone.</typeparam>
+    public interface IDeepCloneable<T>
+    {
+        /// <summary>
+        /// Returns a deep copy of the object.
+        /// </summary>
+        /// <returns>A deep copy of the object.</returns>
+        T DeepClone();
+    }
+}
---
+++ Nightingale.Core/Common/ObservableBase.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Nightingale.Core.Common
+{
+    /// <summary>
+    /// Base class that implements
+    /// INotifyPropertyChanged.
+    /// </summary>
+    public abstract class ObservableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Triggers property changed event.
+        /// </summary>
+        /// <param name="propertyName">Optional. 
+        /// Name of property that changed.
+        /// Uses caller member name by default.</param>
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
