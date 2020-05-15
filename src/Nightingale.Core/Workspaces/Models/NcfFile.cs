632d3a141f57bdcf8863ec029bba14b001526d3f
Daniel Paulino <d_paulino@outlook.com>
2020-05-14 21:17:29 -0700

    Added more models

---
+++ src/Nightingale.Core/Workspaces/Models/NcfFile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace JeniusApps.Nightingale.Core.Workspaces.Models
+{
+    /// <summary>
+    /// The object model for a Nightingale
+    /// Collection Format file.
+    /// </summary>
+    public class NcfFile
+    {
+        /// <summary>
+        /// The list of workspaces in the file.
+        /// </summary>
+        public List<Workspace> Workspaces
+        {
+            get
+            {
+                if (_workspaces == null)
+                {
+                    _workspaces = new List<Workspace>();
+                }
+
+                return _workspaces;
+            }
+            set => _workspaces = value;
+        }
+        private List<Workspace> _workspaces;
+
+        /// <summary>
+        /// List of recently accessed URLs.
+        /// </summary>
+        public List<RecentUrl> RecentUrls
+        {
+            get
+            {
+                if (_recentUrls == null)
+                {
+                    _recentUrls = new List<RecentUrl>();
+                }
+
+                return _recentUrls;
+            }
+            set => _recentUrls = value;
+        }
+        private List<RecentUrl> _recentUrls;
+    }
+}
---
+++ src/Nightingale.Core/Workspaces/Models/RecentUrl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace JeniusApps.Nightingale.Core.Workspaces.Models
+{
+    /// <summary>
+    /// Class representing a URL recently
+    /// used by the user.
+    /// </summary>
+    public class RecentUrl
+    {
+        /// <summary>
+        /// The URL string.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Time when used.
+        /// </summary>
+        public DateTimeOffset Timestamp { get; set; }
+
+        /// <summary>
+        /// Id of parent.
+        /// </summary>
+        public string ParentId { get; set; }    
+    }
+}
