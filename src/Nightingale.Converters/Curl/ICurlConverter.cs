baacded055d62a0746c118504283177eb403ff12
Daniel Paulino <d_paulino@outlook.com>
2020-10-18 00:09:27 -0700

    Added stub for curl converter

---
+++ src/Nightingale.Converters/Curl/ICurlConverter.cs
@@ -0,0 +1,18 @@
+﻿using JeniusApps.Nightingale.Data.Models;
+
+namespace Nightingale.Converters.Curl
+{
+    /// <summary>
+    /// Interface for converting a curl command
+    /// to an <see cref="Item"/>
+    /// </summary>
+    public interface ICurlConverter
+    {
+        /// <summary>
+        /// Converts a curl string to an <see cref="Item"/>.
+        /// </summary>
+        /// <param name="curlString">The curl string to convert.</param>
+        /// <returns>A request or collection in the form of a <see cref="Item"/>.</returns>
+        Item Convert(string curlString);
+    }
+}
